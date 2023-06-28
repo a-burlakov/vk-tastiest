2e59aee5ff01f36bde54f2786ca694389008a249
a-burlakov <fln@mail.ru>
2023-06-28 11:50:58 +0500

    feat: schema descriptions

--- backend/schemas/post.py
+++ backend/schemas/post.py
@@ -1,19 +1,26 @@
 import datetime
 import pydantic as pydantic
+from pydantic import Field
 
 
 class PostPhotos(pydantic.BaseModel):
-    url: str
+    """VK post photos data."""
+
+    url: str = Field(description="Путь URL к фотографии")
 
 
 class PostVideos(pydantic.BaseModel):
-    first_frame_url: str
+    """VK post videos data."""
+
+    first_frame_url: str = Field(description="Путь URL к первому кадру видео")
 
 
 class Post(pydantic.BaseModel):
-    date: datetime.datetime
-    likes: int
-    text: str
-    path: str
-    photos: list[PostPhotos]
-    videos: list[PostVideos]
+    """VK post data."""
+
+    date: datetime.datetime = Field(description="Дата поста")
+    likes: int = Field(description="Количество лайков")
+    text: str = Field(description="Текст поста")
+    path: str = Field(description="Путь URL к посту")
+    photos: list[PostPhotos] = Field(description="Фотографии в посте")
+    videos: list[PostVideos] = Field(description="Видео в посте")
