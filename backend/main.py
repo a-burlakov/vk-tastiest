8d0a7f62b6ed587ea591fbbfdbfda0a251adf177
a-burlakov <fln@mail.ru>
2023-06-28 07:58:22 +0500

    feat: project structure just like Tiangolo does

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from app.api.api_v1.api import api_router
+from app.core.config import settings
 
+app = FastAPI(
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
 
-@app.get("/api")
-async def root():
-    return {"message": "Hello World!"}
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-
-@app.get("/api/wall/")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(api_router, prefix=settings.API_V1_STR)
