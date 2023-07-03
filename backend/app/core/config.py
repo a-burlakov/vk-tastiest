2a5df9305565e4e3b786b71ad7c2a5f5470010b0
a-burlakov <fln@mail.ru>
2023-07-03 10:26:49 +0500

    Chore: docker-compose w/ .env

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -32,9 +32,7 @@ class Settings(BaseSettings):
 
     VKAPI_URL = "https://api.vk.com/method/"
     VKAPI_VERSION = "5.131"
-    VKAPI_TOKEN: str = (
-        "ed52a625ed52a625ed52a6252eee461ffbeed52ed52a62589c8f057c4ad4cf28e7d8a73"
-    )
+    VKAPI_TOKEN: str
 
     LOGGING_STANDARD_PARAMS = {
         "level": logging.INFO,
